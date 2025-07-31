7b3803171d0860214b69b6a39a97669b38164c79
lalit6393 <amanengineer132@gmail.com>
2025-07-31 12:55:31 +0530

    apis

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -11,12 +12,23 @@ const pool = new Pool({
 const app = express();
 app.use(morgan('dev'));
 
+const corsOptions = {
+    origin: 'http://localhost:4000', // Next.js dev server runs on port 3000 by default
+    credentials: true,              // if you send cookies or auth headers
+};
+
+app.use(cors(corsOptions));
+
 app.get('/api/products', async (req, res) => {
+
+    const page = req.params.page || 0;
+    const limit = 100;
+    const offset = page * 100;
     try {
         const client = await pool.connect(); // connect inside the route or in app startup
-        const data = await client.query('SELECT * FROM products');
+        const data = await client.query('SELECT * FROM products LIMIT $1 OFFSET $2', [limit, offset]);
         client.release(); // release connection back to the pool
-        res.json(data.rows); // send data as JSON response
+        res.status(200).json(data.rows); // send data as JSON response
     } catch (error) {
         console.error('Error fetching products:', error);
         res.status(500).send('Server error');
@@ -37,7 +49,7 @@ app.get('/api/products/:id', async (req, res) => {
         );
 
         client.release();
-        res.json(data.rows);
+        res.status(200).json(data.rows);
     } catch (error) {
         console.error('Error fetching product:', error);
         res.status(500).send('Server error');
