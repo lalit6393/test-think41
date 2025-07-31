46b37710d0da5d8d36c7dad6c2defb93caa8dc96
lalit6393 <amanengineer132@gmail.com>
2025-07-31 14:23:23 +0530

    updated query

--- server.js
+++ server.js
@@ -26,7 +26,25 @@ app.get('/api/products', async (req, res) => {
     const offset = page * 100;
     try {
         const client = await pool.connect(); // connect inside the route or in app startup
-        const data = await client.query('SELECT * FROM products LIMIT $1 OFFSET $2', [limit, offset]);
+        const data = await client.query(
+  `SELECT 
+    products.id AS id,
+     products.cost AS cost,
+     products.category AS category,
+     products.brand AS brand,
+     products.name AS name,
+     products.retail_price AS retail_price,
+     products.sku AS sku,
+     products.distribution_center_id AS distribution_center_id,
+     departments.name AS department
+   FROM 
+     products
+   JOIN 
+     departments ON products.department_id = departments.id
+   LIMIT $1 OFFSET $2`,
+  [limit, offset]
+);
+
         client.release(); // release connection back to the pool
         res.status(200).json(data.rows); // send data as JSON response
     } catch (error) {
@@ -44,9 +62,25 @@ app.get('/api/products/:id', async (req, res) => {
         const client = await pool.connect();
 
         const data = await client.query(
-            'SELECT * FROM products WHERE id = $1',
-            [id] // safely parameterized
-        );
+  `SELECT 
+     products.id AS id,
+     products.cost AS cost,
+     products.category AS category,
+     products.brand AS brand,
+     products.name AS name,
+     products.retail_price AS retail_price,
+     products.sku AS sku,
+     products.distribution_center_id AS distribution_center_id,
+     departments.name AS department
+   FROM 
+     products
+   JOIN 
+     departments ON products.department_id = departments.id
+   WHERE 
+     products.id = $1`,
+  [id]
+);
+
 
         client.release();
         res.status(200).json(data.rows);
