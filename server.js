a154cc16be3fe915f27e9d7d36d4853842fec6f2
lalit6393 <amanengineer132@gmail.com>
2025-07-31 14:36:31 +0530

    department api added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/api/products', async (req, res) => {
     try {
         const client = await pool.connect(); // connect inside the route or in app startup
         const data = await client.query(
-  `SELECT 
+            `SELECT 
     products.id AS id,
      products.cost AS cost,
      products.category AS category,
@@ -42,8 +42,8 @@ app.get('/api/products', async (req, res) => {
    JOIN 
      departments ON products.department_id = departments.id
    LIMIT $1 OFFSET $2`,
-  [limit, offset]
-);
+            [limit, offset]
+        );
 
         client.release(); // release connection back to the pool
         res.status(200).json(data.rows); // send data as JSON response
@@ -62,7 +62,7 @@ app.get('/api/products/:id', async (req, res) => {
         const client = await pool.connect();
 
         const data = await client.query(
-  `SELECT 
+            `SELECT 
      products.id AS id,
      products.cost AS cost,
      products.category AS category,
@@ -78,10 +78,73 @@ app.get('/api/products/:id', async (req, res) => {
      departments ON products.department_id = departments.id
    WHERE 
      products.id = $1`,
-  [id]
-);
+            [id]
+        );
+        client.release();
+        res.status(200).json(data.rows);
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).send('Server error');
+    }
+});
 
+app.get('/api/departments', async (req, res) => {
+    try {
+        const client = await pool.connect();
 
+        const data = await client.query(`SELECT * FROM departments`);
+        client.release();
+        res.status(200).json(data.rows);
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/api/departments/:id', async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).send('id not provided');
+
+    try {
+        const client = await pool.connect();
+
+        const data = await client.query(`SELECT * FROM departments WHERE id = $1`, [id]);
+        client.release();
+        res.status(200).json(data.rows);
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/api/departments/:id/products', async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).send('id not provided');
+
+    try {
+        const client = await pool.connect();
+
+        const data = await client.query(
+            `SELECT 
+     products.id AS id,
+     products.cost AS cost,
+     products.category AS category,
+     products.brand AS brand,
+     products.name AS name,
+     products.retail_price AS retail_price,
+     products.sku AS sku,
+     products.distribution_center_id AS distribution_center_id,
+     departments.name AS department
+   FROM 
+     products
+   JOIN 
+     departments ON products.department_id = departments.id
+   WHERE 
+     departments.id = $1`,
+            [id]
+        );
         client.release();
         res.status(200).json(data.rows);
     } catch (error) {
