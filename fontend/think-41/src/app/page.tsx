f0e6dfe88d639eaed3696644c169aef45d471ec6
lalit6393 <amanengineer132@gmail.com>
2025-07-31 15:14:05 +0530

    departments ui

--- fontend/think-41/src/app/page.tsx
+++ fontend/think-41/src/app/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-col gap-4 p-4">
       <p>Welcome home</p>
-      <Link href={'/products'}>Products</Link>
+      <div className="flex gap-4">
+        <Link href={'/products'} className='text-blue-400 bg-slate-100 rounded-lg px-4 py-2'>Products</Link>
+        <Link href={'/departments'} className='text-blue-400 bg-slate-100 rounded-lg px-4 py-2'>Departments</Link>
+      </div>
     </div>
   );
 }
